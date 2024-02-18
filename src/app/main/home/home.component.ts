e8a3b86cc24a577417c87e4383779d541a993259
Harith Kotelawala <harith.kotelawala@gmail.com>
2024-02-18 12:33:59 -0500

    Initial react

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Addon } from '@wealthica/';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+     
+  }
 }
